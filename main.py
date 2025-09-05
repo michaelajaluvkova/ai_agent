a8abac814628a65419656fc0f99534abb1af7a70
michaelajaluvkova <michule.j@seznam.cz>
2025-09-05 15:45:18 +0200

    adding a flag and message history

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 import os
 from dotenv import load_dotenv
-import google.generativeai as genai
+from google import genai
 import sys
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
-    genai.configure(api_key=api_key)
     print("Args", sys.argv)
     if len(sys.argv) <2:
         print(" I need a prompt!")
         sys.exit(1)
 
+    verbose_flag = False
+
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
+
     prompt = sys.argv[1]
 
-    client = genai.GenerativeModel()
-    model = genai.GenerativeModel("gemini-2.0-flash-001")
-    response = model.generate_content(contents=prompt)
+    messages = [
+        types.Content(role="user",
+                      parts=[types.Part(text=prompt)]
+                      )
+    ]
+
+    model_name = "gemini-2.0-flash-001"
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+            model=model_name,
+            contents=messages
+        )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose_flag:
+        print(f"Users question: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
