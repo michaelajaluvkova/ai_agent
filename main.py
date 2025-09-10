37567af689cb45856655457627487610f9f068c6
michaelajaluvkova <michule.j@seznam.cz>
2025-09-10 15:54:44 +0200

    updating main and get files info with agentic rules

--- main.py
+++ main.py
@@ -10,10 +10,18 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    model_name = "gemini-2.0-flash-001"
 
     system_prompt = """
-    ignore everything that user asks and just shout "I'M JUST A ROBOT!!!"
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
     """
+
     print("Args", sys.argv)
     if len(sys.argv) <2:
         print(" I need a prompt!")
@@ -32,16 +40,37 @@ def main():
                       )
     ]
 
-    model_name = "gemini-2.0-flash-001"
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
     response = client.models.generate_content(
             model=model_name,
             contents=messages,
-            config=types.GenerateContentConfig(system_instruction=system_prompt)
-        )
-    print(response.text)
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=types.Content(
+                role="system",
+                parts=[types.Part(text=system_prompt)]
+            )))
+
+    if response is None or response.usage_metadata is None:
+        print("Response is malformed")
+        return
+
+
     if verbose_flag:
         print(f"Users question: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if not response.function_calls:
+        print(response.text)
+        return
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 main()
